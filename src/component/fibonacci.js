ddd6dd80c3c4aecc8747123c368f48b3f294a0eb
Sylvain Gogel <sylvain.gogel@gmail.com>
2019-02-02 21:19:23 +0100

    fix: fix jsdoc

--- src/component/fibonacci.js
+++ src/component/fibonacci.js
@@ -1,20 +1,26 @@
 /**
- * @usage const a = [...fibonacci(20)]
+ * @usage const a = fibonacci(20)
  *
- * @param int n
- * @param int current
- * @param int next
+ * @param int n length of the fibonacci suite
+ * @returns Array
+ * 
+ * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Spread_syntax
  */
-
 function fibonacci(n) {
+  //doing spread operator on iterable will generate the array
   return [...fibonacciRaw(n)]
 }
 
 /**
- *
- * @param int n  positive integer
+ * private function that do the trick
+ * 
+ * it use functionnal recusion and generator
+ * @see https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Instructions/function*
+ * 
+ * @param int n 
  * @param {*} current
  * @param {*} next
+ * @returns iterable 
  */
 function* fibonacciRaw(n, current = 0, next = 1) {
   //Condition de fin de l'appel reccursif
